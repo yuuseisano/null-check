4c06fc8ce186bca6a5254e1b58a753dde2cca614
4D-19\koyo <a>
2026-06-09 13:56:40 +0900

    shared_ptrを使った設計

--- null check/null check.cpp
+++ null check/null check.cpp
@@ -2,26 +2,55 @@
 #include<assert.h>
 
 
-class Test
-{
-public:
-	void Show()
-	{
-		std::cout << "Hello World!" << std::endl;
-	}
+class Weapon {
 
-
+	public:
+		void Use() {}
 };
 
-void Func(Test* test)
-{
-	assert(test != nullptr);
-	test->Show();
+class Player {
+	std::shared_ptr<Weapon>weapon;
+public:
+	Player(std::shared_ptr<Weapon>weapon1) :weapon(std::move(weapon1)) {
+		std::cout << weapon.use_count() << std::endl;
+		std::cout << weapon1.use_count() << std::endl;
+	}
+	void Attack() {
+		if (weapon)weapon->Use();
+	}
+};
+
+int main() {
+	auto wp = std::make_shared<Weapon>();
+
+	Player p(wp);
+	Player q(std::move(wp));
+
+	return 0;
 }
 
+//class Test
+//{
+//public:
+//	void Show()
+//	{
+//		std::cout << "Hello World!" << std::endl;
+//	}
+//
+//
+//};
+//
+//void Func(Test* test)
+//{
+//	assert(test != nullptr);
+//	test->Show();
+//}
+//
+//
+//int main()
+//{
+//	Func(new Test());
+//	Func(nullptr);
+//}
 
-int main()
-{
-	Func(new Test());
-	Func(nullptr);
-}
+
